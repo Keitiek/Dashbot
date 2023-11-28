9ccbfde077d26b3f3b122859f544fa74af407f8e
FrenchFriesForBallerina <9962944+FrenchFriesForBallerina@users.noreply.github.com>
2023-11-28 15:01:32 +0200

    RC code working: throttle and auto stop, gradual speedup, turning ok

--- RC/Untitled-1.py
+++ RC/Untitled-1.py
@@ -1,63 +1,151 @@
- 
-#  Arduino FS-I6X Demo
- # fsi6x-arduino-mega-ibus.ino
-  #Read iBus output port from FS-IA6B receiver module
-#  Display values on Serial Monitor
- 
- # Channel functions by Ricardo Paiva - https://gist.github.com/werneckpaiva/
- 
-  #DroneBot Workshop 2021
-  #https://dronebotworkshop.com
-
- 
-// Include iBusBM Library
 #include <IBusBM.h>
- 
-// Create iBus Object
+
+// channels on physical RC:
+// 1 - left (negative), right (positive)
+// 3 - break (negative), throttle (positive)
+// 5 - SWD up (negative), SWD down (positive)
+// 6 - LIGHTS OFF (negative), ON (positive)
+// 8 - FWD (negative), REV (positive)
+
+const long baudRate = 115200;
+
+struct Speed {
+  int left;
+  int right;
+};
+
+Speed currentSpeed;
+
+const int throttlePinRight = 2; 
+const int throttlePinLeft = 11;
+const int cruisePinRight = 7;
+const int cruisePinLeft = 5;
+
+const int leftRightChannel = 0;
+const int breakThrottleChannel = 2;
+const int lightsOffOnChannel = 5; // (-100, 100)
+const int forwardReverseChannel = 7; // (-100, 0, 100)
+
+const int numberOfChannelsWeAreInterestedIn = 7;
+
 IBusBM ibus;
- 
+
 // Read the number of a given channel and convert to the range provided.
 // If the channel is off, return the default value
 int readChannel(byte channelInput, int minLimit, int maxLimit, int defaultValue) {
-  uint16_t ch = ibus.readChannel(channelInput);
-  if (ch < 100) return defaultValue;
-  return map(ch, 1000, 2000, minLimit, maxLimit);
+  uint16_t readValue = ibus.readChannel(channelInput);
+  if (readValue < 100) return defaultValue;
+  return map(readValue, 1000, 2000, minLimit, maxLimit);
 }
- 
-// Read the channel and return a boolean value
-bool readSwitch(byte channelInput, bool defaultValue) {
-  int intDefaultValue = (defaultValue) ? 100 : 0;
-  int ch = readChannel(channelInput, 0, 100, intDefaultValue);
-  return (ch > 100);
-}
- 
+
 void setup() {
-  // Start serial monitor
-  Serial.begin(9600);
- 
+  Serial.begin(baudRate);
+  pinMode(cruisePinRight, OUTPUT); 
+  pinMode(cruisePinLeft, OUTPUT); 
   // Attach iBus object to serial port
   ibus.begin(Serial1);
+
+  currentSpeed.left = 0;
+  currentSpeed.right = 0;
 }
- 
-void loop() {
- 
-  // Cycle through first 5 channels and determine values
-  // Print values to serial monitor
-  // Note IBusBM library labels channels starting with "0"
- 
-  for (byte i = 0; i < 5; i++) {
-    int value = readChannel(i, -100, 100, 0);
-    Serial.print("Ch");
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.print(value);
-    Serial.print(" | ");
+
+int lastTurningValue = 0;
+int currentTurningValue = 0;
+
+void loop() {  
+  for (byte channel = 0; channel <= numberOfChannelsWeAreInterestedIn; channel++) {
+    
+    int value = readChannel(channel, -100, 100, 0);
+    
+    // Throttle and break
+    if(channel == breakThrottleChannel){
+      if (value >= 0){
+        forward(value);
+      } 
+    } 
+
+     if (channel == leftRightChannel){
+      if (value != 0) {
+        adjustTurning(value);
+        sendSpeedSignalToMotors();
+      }
+    }
+     
+    // Driving forward or reversing
+    /*
+    if(channel == forwardReverseChannel) {
+      if(value > 0){
+        reverse();
+      } else if(value == 0) {
+        Serial.print("NEUTRAL | ");
+      } else{
+        Serial.print("FORWARD | ");
+      }
+    }
+    */
+    // Turning lights on and off 
+
+    /*
+    if(channel == lightsOffOnChannel){
+      if(value < 0){
+       // Serial.print("LIGHTS OFF | ");
+      } else {
+        Serial.print("LIGHTS ON | ");
+      }
+    }
+    */
   }
- 
-  // Print channel 6 (switch) boolean value
-  Serial.print("Ch6: ");
-  Serial.print(readSwitch(5, false));
-  Serial.println();
- 
+  sendSpeedSignalToMotors();
   delay(10);
 }
+
+void adjustTurning(int value) {
+  int turnSpeedLeft = 0;
+  int turnSpeedRight = 0;
+  if(value < 0){
+    turnSpeedLeft = map(value, 0, -100, 0, 255);
+    currentSpeed.right += (turnSpeedLeft );
+  } else {
+    turnSpeedRight = map(value, 0, 100, 0, 255);
+    currentSpeed.left += (turnSpeedRight);
+  }
+    
+  currentSpeed.left = constrain(currentSpeed.left, 0, 255);
+  currentSpeed.right = constrain(currentSpeed.right, 0, 255);
+}
+
+void forward(int value){
+  int leftSpeed = map(value, 0, 100, 0, 255);
+  int rightSpeed = map(value, 0, 100, 0, 255);
+  currentSpeed.right = rightSpeed; 
+  currentSpeed.left = leftSpeed;
+}
+
+void sendSpeedSignalToMotors(){
+  digitalWrite(cruisePinRight, LOW);
+  digitalWrite(cruisePinLeft, LOW);
+  
+  Serial.print("RIGHT value | ");
+  Serial.println(currentSpeed.right);
+
+  Serial.print("LEFT value | ");
+  Serial.println(currentSpeed.left);
+  
+  analogWrite(throttlePinRight, currentSpeed.right);
+  analogWrite(throttlePinLeft, currentSpeed.left);  
+}
+
+// braking at speed 0 happens because of BAC settings (activated in kilowatt app). 
+// If the throttle has a low value, the break applies automatically.
+ 
+// todo: modify to use reverse
+void reverse(int value){
+  Serial.print("REVERSE | ");
+  int leftSpeed = map(value, 0, -100, 0, -255);
+  int rightSpeed = map(value, 0, -100, 0, -255);
+  currentSpeed.right = rightSpeed; 
+  currentSpeed.left = leftSpeed;
+}
+
+// next steps in hardware: wires repairs, jetson pocket, waterproof the electronics, base for cargo, cargo ties 
+// next steps in code: apply reverse, teleop: control over ros, attach camera
