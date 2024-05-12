91e0883c44392912fb7e779a618fc2904821ea75
FrenchFriesForBallerina <9962944+FrenchFriesForBallerina@users.noreply.github.com>
2024-05-12 20:41:28 +0300

    Create PinDefinitions.hpp

---
+++ arduino/PinDefinitions.hpp
@@ -0,0 +1,9 @@
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+const int throttlePinLeft = 11; 
+const int throttlePinRight = 2;
+const int cruisePinLeft = 5;
+const int cruisePinRight = 7;
+
+#endif // PIN_DEFINITIONS_H
