6435a59dc2399e171937d68a3c41680888c08428
jormroots <56258874+jormroots@users.noreply.github.com>
2024-12-10 11:13:06 +0200

    Update lane_detection.py

--- camera/Camera/lane_detection.py
+++ camera/Camera/lane_detection.py
@@ -43,5 +43,5 @@ def draw_lane_lines(image, lines, color=[255, 255, 0], thickness=12):
             if isinstance(pt1, tuple) and isinstance(pt2, tuple):
                 pt1 = tuple(map(int, pt1))
                 pt2 = tuple(map(int, pt2))
-                cv2.line(line_image, pt1, pt2, color, thickness)
+                #cv2.line(line_image, pt1, pt2, color, thickness)
     return cv2.addWeighted(image, 1.0, line_image, 1.0, 0.0)
